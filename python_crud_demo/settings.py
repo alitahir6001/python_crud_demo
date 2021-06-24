2233491072b9848bdb199c525bdc79cf07824280
Ali <alitahir6001@gmail.com>
2021-06-24 17:03:15 -0400

    Update settings.py

--- python_crud_demo/settings.py
+++ python_crud_demo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=e1ea-rs+ms=qbhmm+q*(0v%*+w&l65+-1%#9hgh!s7v+xi%!e'
+SECRET_KEY = 'ah ah ah, you didnt say the magic word'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
